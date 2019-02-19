7ca225f370698b7c58290c3e6ba087ab007319c4
Devon Marantz <devonmarantz@gmail.com>
2019-02-19 10:56:14 -0800

    Add save button
    Resolves #4

---
+++ js/main.js
@@ -0,0 +1,8 @@
+<<<<<<< Updated upstream
+=======
+import toggleActive from './toggleActive.js'
+
+//Select the edit buttons
+//Add event listener of the click edit button
+document.querySelectorAll('.edit-button, .save-button').forEach(button => button.addEventListener('click', toggleActive));
+>>>>>>> Stashed changes
